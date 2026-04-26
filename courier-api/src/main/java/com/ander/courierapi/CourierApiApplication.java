8dc40f75f2e57aa72eefdadb3e1a70d6a5046599
anderlya <acevedo.anderson@gmail.com>
2026-04-26 13:29:30 -0500

    Correcion paquete api main

--- courier-api/src/main/java/com/ander/courierapi/CourierApiApplication.java
+++ courier-api/src/main/java/com/ander/courierapi/CourierApiApplication.java
@@ -1,4 +1,4 @@
-package com.ander.courierapi.courier_api;
+package com.ander.courierapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
