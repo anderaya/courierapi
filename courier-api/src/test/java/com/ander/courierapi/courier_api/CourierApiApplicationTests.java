769b784fb7c6fc1d1bb11a3302adea1a31f5c6a3
anderlya <acevedo.anderson@gmail.com>
2026-04-26 11:41:14 -0500

    inicializar proyecto Spring Boot con dependencias base

---
+++ courier-api/src/test/java/com/ander/courierapi/courier_api/CourierApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ander.courierapi.courier_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
